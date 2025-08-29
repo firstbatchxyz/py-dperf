b947cbc5eba66e4d94225f8fe87683fcbbd7df17
Octavian Rusu <octavian@firstbatch.xyz>
2025-08-29 01:40:59 -0700

    separate medatada obj from deepseek-specific implementation

---
+++ src/parsers/meta.py
@@ -0,0 +1,24 @@
+
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class LayerMeta:
+    name: str = ""            # Name of the symbol 
+    submodules: Any = None    # Submodules decomposed into LayerMeta
+    parent_layer: Any = None  # Parent Compount Layer 
+    layer: Any = None         # Original object  
+    flops: float = 0.0        # Estimated FLOPs to compute
+    weight_bytes: int = 0     # Bytes of internal weight tensor
+    input_bytes: int  = 0     # Bytes of input tensor 
+    output_bytes: int = 0     # Bytes of output tensor
+    kv_cache_t: int  = 0      # Total tokens stored in KV Cache
+    kv_cache_r: int  = 0      # Bytes of KV Cache read 
+    kv_cache_w: int  = 0      # Bytes of KV Cache written
+    ram_vram_rw: int = 0      # Bytes of data transmitted between RAM <-> VRAM 
+
+    def __repr__(self):
+        return (f"<LayerMeta {self.name}: "
+                f"FLOPs={self.flops}, INPUT={self.input_bytes}, OUTPUT={self.output_bytes}, "
+                f"WEIGHT={self.weight_bytes}, parent={self.parent_layer.__class__.__name__}" )
+
--- src/parsers/mlx_deepseek.py
+++ src/parsers/mlx_deepseek.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 from typing import Any
 from mlx_lm.models.deepseek_v2 import DeepseekV2DecoderLayer
 
+from src.parsers.meta import LayerMeta
+
 #from src.model.base import IdentityBlock 
 
 """
@@ -14,27 +16,6 @@ from mlx_lm.models.deepseek_v2 import DeepseekV2DecoderLayer
     NOTE: Small OPs like RoPE and norms default to 0 (like prima.cpp)
     NOTE: FMA default to 2 FLOPs
 """
-
-@dataclass
-class LayerMeta:
-    name: str = ""            # Name of the symbol 
-    submodules: Any = None    # Submodules decomposed into LayerMeta
-    parent_layer: Any = None  # Parent Compount Layer 
-    layer: Any = None         # Original object  
-    flops: float = 0.0        # Estimated FLOPs to compute
-    weight_bytes: int = 0     # Bytes of internal weight tensor
-    input_bytes: int  = 0     # Bytes of input tensor 
-    output_bytes: int = 0     # Bytes of output tensor
-    kv_cache_t: int  = 0      # Total tokens stored in KV Cache
-    kv_cache_r: int  = 0      # Bytes of KV Cache read 
-    kv_cache_w: int  = 0      # Bytes of KV Cache written
-    ram_vram_rw: int = 0      # Bytes of data transmitted between RAM <-> VRAM 
-
-    def __repr__(self):
-        return (f"<LayerMeta {self.name}: "
-                f"FLOPs={self.flops}, INPUT={self.input_bytes}, OUTPUT={self.output_bytes}, "
-                f"WEIGHT={self.weight_bytes}, parent={self.parent_layer.__class__.__name__}" )
-
 
 # Give batch size and sequence length
 def _profile_model(m: nn.Module, config: Any, B:int = 1, L:int = 4096, a_dtype=mx.float16, w_dtype=mx.float32):
